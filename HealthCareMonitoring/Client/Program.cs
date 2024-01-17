10b188bce1d3911ae976e94abf60f0690dfb83d3
HZY <heng.zengyang@gmail.com>
2024-01-17 09:48:28 +0800

    Added components, services and endpoint

--- HealthCareMonitoring/Client/Program.cs
+++ HealthCareMonitoring/Client/Program.cs
@@ -1,17 +1,25 @@
 using HealthCareMonitoring.Client;
+using HealthCareMonitoring.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("HealthCareMonitoring.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddHttpClient("HealthCareMonitoring.ServerAPI", (sp, client) => {
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+    client.EnableIntercept(sp);
+})
+.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
-// Supply HttpClient instances that include access tokens when making requests to the server project
+//Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("HealthCareMonitoring.ServerAPI"));
+
+builder.Services.AddHttpClientInterceptor();
+builder.Services.AddScoped<HttpInterceptorService>();
 
 builder.Services.AddApiAuthorization();
 
