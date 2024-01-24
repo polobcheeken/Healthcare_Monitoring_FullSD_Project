d5c421ab740e93d94f6eca48c867d20c59f80982
TAN HAO WEN JOEL 2204163F <2204163F@student.tp.edu.sg>
2024-01-25 02:19:17 +0800

    Seeded additional entities and debugged for required field. Adding requried field next commit.

--- HealthCareMonitoring/Server/Configurations/Entities/HospitalSeedConfiguration.cs
+++ HealthCareMonitoring/Server/Configurations/Entities/HospitalSeedConfiguration.cs
@@ -4,49 +4,53 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HealthCareMonitoring.Server.Configurations.Entities
 {
-	public class HospitalSeedConfiguration : IEntityTypeConfiguration<Hospital>
-	{
-		public void Configure(EntityTypeBuilder<Hospital> builder)
-		{
-			builder.HasData(
-			new Hospital
-			{
-				Id = 1,
-				Name = "KK Women’s and Children’s Hospital",
-				Address = "100 Bukit Timah Road",
-				PostalCode = "Singapore 229899",
-                DateCreated = DateTime.Now,
-				DateUpdated = DateTime.Now,
-				CreatedBy = "System",
-				UpdatedBy = "System"
-			},
-			new Hospital
-			{
-				Id = 2,
-				Name = "Singapore General Hospital",
-				Address = "Bukit Merah",
-                PostalCode = "Singapore 169608",
-                DateCreated = DateTime.Now,
-				DateUpdated = DateTime.Now,
-				CreatedBy = "System",
-				UpdatedBy = "System"
-			},
-			new Hospital
-			{
-				Id = 3,
-				Name = "Gleneagles Hospital",
-				Address = "6A Napier Road",
-				PostalCode = "Singapore 258500",
-                DateCreated = DateTime.Now,
-				DateUpdated = DateTime.Now,
-				CreatedBy = "System",
-				UpdatedBy = "System"
-			},
+    public class HospitalSeedConfiguration : IEntityTypeConfiguration<Hospital>
+    {
+        public void Configure(EntityTypeBuilder<Hospital> builder)
+        {
+            builder.HasData(
             new Hospital
             {
-				Id = 4,
+                Id = 1,
+                Name = "KK Women’s and Children’s Hospital",
+                Address = "100 Bukit Timah Road",
+                Description = "Specialising in obstetrics, gynaecology, neonatology and paediatrics",
+                PostalCode = "Singapore 229899",
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                CreatedBy = "System",
+                UpdatedBy = "System"
+            },
+            new Hospital
+            {
+                Id = 2,
+                Name = "Singapore General Hospital",
+                Address = "Bukit Merah",
+                Description = "Larget tertiary hospital and ranked among the world's best",
+                PostalCode = "Singapore 169608",
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                CreatedBy = "System",
+                UpdatedBy = "System"
+            },
+            new Hospital
+            {
+                Id = 3,
+                Name = "Gleneagles Hospital",
+                Address = "6A Napier Road",
+                Description = "Private institution with comprehensive range of medical care",
+                PostalCode = "Singapore 258500",
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                CreatedBy = "System",
+                UpdatedBy = "System"
+            },
+            new Hospital
+            {
+                Id = 4,
                 Name = "Changi General Hospital",
                 Address = "2 Simei Street 3",
+                Description = "1000-bed hospital, always alwayble for emergencies",
                 PostalCode = "Singapore 529889",
                 DateCreated = DateTime.Now,
                 DateUpdated = DateTime.Now,
@@ -58,13 +62,14 @@ namespace HealthCareMonitoring.Server.Configurations.Entities
                 Id = 5,
                 Name = "Mount Elizabeth Hospital",
                 Address = "3 Mount Elizabeth",
-				PostalCode = "Singapore 228510",
+                Description = "Singapore's best doctors and provides exceptional personalised medical services",
+                PostalCode = "Singapore 228510",
                 DateCreated = DateTime.Now,
                 DateUpdated = DateTime.Now,
                 CreatedBy = "System",
                 UpdatedBy = "System"
             }
             );
-		}
-	}
+        }
+    }
 }
--- HealthCareMonitoring/Server/Configurations/Entities/MedicineSeedConfiguration.cs
+++ HealthCareMonitoring/Server/Configurations/Entities/MedicineSeedConfiguration.cs
@@ -4,53 +4,53 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HealthCareMonitoring.Server.Configurations.Entities
 {
-	public class MedicineSeedConfiguration : IEntityTypeConfiguration<Medicine>
-	{
-		public void Configure(EntityTypeBuilder<Medicine> builder)
-		{
-			builder.HasData(
-			new Medicine
-			{
-				Id = 1,
-				Name = "Paracetamol",
-				Description = "Used for headaches",
-				Type = "Tablet",
-				MedicineFee = 5,
-                ManufacturedDate = null,
-				ExpiryDate = null,
-				DateCreated = DateTime.Now,
-				DateUpdated = DateTime.Now,
-				CreatedBy = "System",
-				UpdatedBy = "System"
-			},
-			new Medicine
-			{
-				Id = 2,
-				Name = "Ibuprofen",
-				Description = "Strong Painkiller, Only used when needed",
-				Type = "Tablet",
-				MedicineFee = 5.50,
-                ManufacturedDate = null,
-				ExpiryDate = null,
-				DateCreated = DateTime.Now,
-				DateUpdated = DateTime.Now,
-				CreatedBy = "System",
-				UpdatedBy = "System"
-			},
-			new Medicine
-			{
-				Id = 3,
-				Name = "Xanax",
-				Description = "Used to treat anxiety disorders",
-				Type = "Pill",
-				MedicineFee = 7.40,
-                ManufacturedDate = null,
-				ExpiryDate = null,
-				DateCreated = DateTime.Now,
-				DateUpdated = DateTime.Now,
-				CreatedBy = "System",
-				UpdatedBy = "System"
-			},
+    public class MedicineSeedConfiguration : IEntityTypeConfiguration<Medicine>
+    {
+        public void Configure(EntityTypeBuilder<Medicine> builder)
+        {
+            builder.HasData(
+            new Medicine
+            {
+                Id = 1,
+                Name = "Paracetamol",
+                Description = "Used for headaches",
+                Type = "Tablet",
+                MedicineFee = 5,
+                ManufacturedDate = new DateTime(01 / 01 / 2024),
+                ExpiryDate = new DateTime(01 / 01 / 2030),
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                CreatedBy = "System",
+                UpdatedBy = "System"
+            },
+            new Medicine
+            {
+                Id = 2,
+                Name = "Ibuprofen",
+                Description = "Strong Painkiller, Only used when needed",
+                Type = "Tablet",
+                MedicineFee = 5.50,
+                ManufacturedDate = new DateTime(01 / 01 / 2024),
+                ExpiryDate = new DateTime(01 / 01 / 2030),
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                CreatedBy = "System",
+                UpdatedBy = "System"
+            },
+            new Medicine
+            {
+                Id = 3,
+                Name = "Xanax",
+                Description = "Used to treat anxiety disorders",
+                Type = "Pill",
+                MedicineFee = 7.40,
+                ManufacturedDate = new DateTime(01 / 01 / 2024),
+                ExpiryDate = new DateTime(01 / 01 / 2030),
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                CreatedBy = "System",
+                UpdatedBy = "System"
+            },
             new Medicine
             {
                 Id = 4,
@@ -58,8 +58,8 @@ namespace HealthCareMonitoring.Server.Configurations.Entities
                 Description = "Used to numb the throat and lungs to make cough reflex less active",
                 Type = "Pill",
                 MedicineFee = 4.30,
-                ManufacturedDate = null,
-                ExpiryDate = null,
+                ManufacturedDate = new DateTime(01 / 01 / 2024),
+                ExpiryDate = new DateTime(01 / 01 / 2030),
                 DateCreated = DateTime.Now,
                 DateUpdated = DateTime.Now,
                 CreatedBy = "System",
@@ -72,14 +72,14 @@ namespace HealthCareMonitoring.Server.Configurations.Entities
                 Description = "Used to reduce swelling and itching of allergic reactions",
                 Type = "Cream",
                 MedicineFee = 13.20,
-                ManufacturedDate = null,
-                ExpiryDate = null,
+                ManufacturedDate = new DateTime(01 / 01 / 2024),
+                ExpiryDate = new DateTime(01 / 01 / 2030),
                 DateCreated = DateTime.Now,
                 DateUpdated = DateTime.Now,
                 CreatedBy = "System",
                 UpdatedBy = "System"
             }
             );
-		}
-	}
+        }
+    }
 }
